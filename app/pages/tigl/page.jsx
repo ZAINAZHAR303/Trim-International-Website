abcedf9c1d6ed8316fc214448022f20c40ab048f
zain azhar <142168421+ZAINAZHAR303@users.noreply.github.com>
2025-04-20 16:05:07 +0500

    make the tigl page

---
+++ app/pages/tigl/page.jsx
@@ -0,0 +1,14 @@
+import Development from '@/app/components/Development'
+import Poster from '@/app/components/poster/Poster'
+import React from 'react'
+
+const Tigl = () => {
+  return (
+    <>
+     <Poster />
+     <Development />
+    </>
+  )
+}
+
+export default Tigl
